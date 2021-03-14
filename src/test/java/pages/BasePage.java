edb0f13001a49423dbdb8c5bb00db1be79c536cd
sujitha <sujithalakshmi.sundar@gmail.com>
2021-03-15 03:42:40 +0530

    parameterize the prices and category

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class BasePage {
 
     WebDriver driver;
@@ -55,8 +57,9 @@ public class BasePage {
         return Element.getText();
     }
 
+    public Integer ConvertPriceTagIntoNumber(String BasePrice){
 
-
-
+        return 1;
+    }
 
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Reporter;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -24,6 +26,96 @@ public class HomePage extends BasePage {
 
     @FindAll(@FindBy(xpath = "//*[contains(@id,'product_')]//a"))
     List<WebElement> Search_List;
+
+    @FindAll(@FindBy(xpath="//a[@class='list-group-item'"))
+    List<WebElement> ShopByCategory;
+
+    @FindAll(@FindBy(xpath="//*[@name='search[price_range_any][]']"))
+    List<WebElement>  PriceRanges;
+
+    @FindBy(xpath = "//*[contains(@class,'btn-primary')]")
+    WebElement btnSearchByCategory;
+
+    @FindAll(@FindBy(xpath="//*[@itemprop='price']"))
+    List<WebElement>  SearchCriteriaResults;
+
+
+    public void selectShopByCategory(String Category){
+
+        for (WebElement category: ShopByCategory) {
+            if(category.getText().equalsIgnoreCase(Category)){
+                category.click();
+                break;
+            }
+        }
+        WaitUntilElementIsVisibleBy("xpath","//*[contains(@class,'filter-title')]");
+    }
+
+    public void selectPriceRange(String Price){
+        for (WebElement price: PriceRanges) {
+            if(price.getAttribute("value").equalsIgnoreCase(Price)){
+                price.click();
+                break;
+            }
+        }
+    }
+
+    public void SearchByCategoryAndPrice(String Category,String Price){
+        selectShopByCategory(Category);
+        selectPriceRange(Price);
+        btnSearchByCategory.click();
+    }
+
+    public Boolean verifyPriceTagSearchResults(String PriceRange) {
+
+        for (WebElement searchItem : SearchCriteriaResults) {
+            String ItemBasePrice = searchItem.getAttribute("content").split(".")[0];
+            Integer BasePrice = Integer.parseInt(ItemBasePrice);
+
+            String[] priceSplit = PriceRange.split("$");
+            String value = null;
+            Integer MinPrice = 0, MaxPrice = 0;
+
+            for (String splitValue : priceSplit) {
+
+                value = value.substring(0, 2);
+
+                try {
+
+                    if (MinPrice == 0) {
+                        MinPrice = Integer.parseInt(value);
+                    } else {
+                        MaxPrice = Integer.parseInt(value);
+                    }
+                } catch (Exception e) {
+                    //Do Nothing
+                }
+
+                if(BasePrice>=MinPrice && MinPrice <=MaxPrice){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+
+                }
+
+            }
+
+            return false;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
     public Boolean getResultsWithSearchText(String SearchForProduct){
 
